5539cacf4888aeb438280561bfa1dbb3f8ec2790
fatih <faticetinnn@gmail.com>
2023-07-10 14:34:36 +0300

    2. commit

---
+++ Proje1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje1
+{
+    internal class Program
+    {
+        enum Days
+        {
+            Mondey,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+        }
+        static void Main(string[] args)
+        {
+            //char i = 'b';
+            //Console.WriteLine((int)i);
+
+
+            Console.WriteLine(Days.Tuesday);
+
+
+            Console.ReadLine();
+        }
+    }
+}
