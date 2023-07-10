a22c1d655684d84ee79415572d0729f544822190
fatih <faticetinnn@gmail.com>
2023-07-10 15:54:36 +0300

    metotlar

---
+++ Metotlar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Add();
+            Add();
+
+            var result = Add2(20,30);
+
+            Console.WriteLine(result);
+
+
+   
+            Console.ReadKey();
+
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("ADDDDDDD!");
+        }
+        static int Add2(int nummber1 , int number2)
+        {
+           int result = nummber1 + number2;
+
+            return result;
+        }
+    }
+}
